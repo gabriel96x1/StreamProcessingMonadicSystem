36ce3d585efa662c0758d96de1961a2bd33faaee
Gabriel RZ <gabriel96x1@gmail.com>
2025-03-02 21:00:51 -0600

    added test application in main

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from src.py_text_analyzer import PyTextAnalyzer
 from src.data_requester import request_reddit_data
+import random
+import asyncio
 
-if __name__ == "__main__":
+async def main():
     processor = PyTextAnalyzer()
 
-    comments = request_reddit_data(25)
+    #comments = request_reddit_data(30)
+    for i in range(5):
+        with open('comments_to_read.txt', 'r') as f:
 
-    for comment in comments:
-        text = processor.process(comment.body).get()
-        print(text)
+            for line in f.readlines():
+                text = processor.process(line).get()
+                print(text)
+
+            rn = random.choice([7,8,9,10,1])
+            print(f"time to sleep {rn}")
+            await asyncio.sleep(rn)
+
+if __name__ == "__main__":
+    asyncio.run(main())
